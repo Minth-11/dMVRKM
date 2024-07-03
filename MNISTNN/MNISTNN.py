00e0f54e793cbaaec8fd2e899acfead0814aa8cb
Zander Op de Beeck <zander.opdebeeck@gmail.com>
2024-07-03 16:45:42 +0200

    working model - basic

--- MNISTNN/MNISTNN.py
+++ MNISTNN/MNISTNN.py
@@ -1,5 +1,99 @@
 import torch
+import torch.nn as nn
+from tqdm import tqdm
 from torchvision import datasets, transforms
+from dataclasses import dataclass
+from torch.utils.data import DataLoader
+import torchvision.transforms as T
+from torch.autograd import Variable
 
-dataset1 = datasets.MNIST('../data', train=True,  download=True)
-dataset2 = datasets.MNIST('../data', train=False, download=True)
+device = (
+    "cuda"
+    if torch.cuda.is_available()
+    else "mps"
+    if torch.backends.mps.is_available()
+    else "cpu"
+)
+print(f"Using {device} device")
+
+def main():
+    train_data = datasets.MNIST('../data', train=True,  download=True, transform=T.ToTensor())
+    test_data = datasets.MNIST('../data', train=False, download=True, transform=T.ToTensor())
+    loaders = {
+    'train' : torch.utils.data.DataLoader(train_data,
+                                          batch_size=100,
+                                          shuffle=True,
+                                          num_workers=1),
+
+    'test'  : torch.utils.data.DataLoader(test_data,
+                                          batch_size=100,
+                                          shuffle=True,
+                                          num_workers=1),
+    }
+
+    model = Model()
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+    nEpochs = 10
+    train(nEpochs, model, loaders, criterion, optimizer)
+    test(model,loaders)
+
+def train(nEpochs, model, loaders, loss_func, optimizer):
+    model.train()
+    total_step = len(loaders['train'])
+    for epoch in range(nEpochs):
+        for i, (images, labels) in enumerate(loaders['train']):
+            # gives batch data, normalize x when iterate train_loader
+            b_x = Variable(images)   # batch x
+            b_y = Variable(labels)   # batch y
+            output = model(b_x)[0]
+            loss = loss_func(output, b_y)
+            # clear gradients for this training step
+            optimizer.zero_grad()
+            # backpropagation, compute gradients
+            loss.backward()                # apply gradients
+            optimizer.step()
+            if (i+1) % 100 == 0:
+                print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'
+                       .format(epoch + 1, nEpochs, i + 1, total_step, loss.item()))
+
+def test(model,loaders):
+    model.eval()
+    with torch.no_grad():
+        correct = 0
+        total = 0
+        for images, labels in loaders['test']:
+            test_output, last_layer = model(images)
+            pred_y = torch.max(test_output, 1)[1].data.squeeze()
+            accuracy = (pred_y == labels).sum().item() / float(labels.size(0))
+    print('Test Accuracy of the model on the 10000 test images: %.2f' % accuracy)
+
+class Model(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=1,
+                out_channels=16,
+                kernel_size=5,
+                stride=1,
+                padding=2,
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(16, 32, 5, 1, 2),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+        )        # fully connected layer, output 10 classes
+        self.out = nn.Linear(32 * 7 * 7, 10)
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)        # flatten the output of conv2 to (batch_size, 32 * 7 * 7)
+        x = x.view(x.size(0), -1)
+        output = self.out(x)
+        return output, x    # return x for visualization
+
+if __name__ == '__main__':
+    main()
